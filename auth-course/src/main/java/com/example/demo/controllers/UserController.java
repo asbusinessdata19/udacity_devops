cabcba82090ed1c48092cdad3e8d645f2ab51279
asaleh <asaleh@NBA-ASALEH>
2021-07-25 20:11:29 +0200

    Changes
    Signed-off-by: asaleh <asaleh@NBA-ASALEH>

--- auth-course/src/main/java/com/example/demo/controllers/UserController.java
+++ auth-course/src/main/java/com/example/demo/controllers/UserController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +22,8 @@ import com.example.demo.model.requests.CreateUserRequest;
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+	
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
 	
 	@Autowired
 	BCryptPasswordEncoder bCryptPasswordEncoder;
@@ -44,10 +48,16 @@ public class UserController {
 	@PostMapping("/create")
 	public ResponseEntity<Object> createUser(@RequestBody CreateUserRequest createUserRequest) {
 		if(createUserRequest.getPassword1().isEmpty()) {
+			log.error("Error Creating user "+createUserRequest.getUsername()+ ": Password cann't be empty");
 			return new ResponseEntity<Object>("Password cann't be empty",HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		if(!createUserRequest.getPassword1().equals(createUserRequest.getPassword2())) {
+			log.error("Error Creating user "+createUserRequest.getUsername()+ ": Provided passwords are not matched");
 			return new ResponseEntity<Object>("Provided passwords are not matched",HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		if(createUserRequest.getPassword1().length()<=8) {
+			log.error("Error Creating user "+createUserRequest.getUsername()+ ": password length must be higher than 8 characters");
+			return new ResponseEntity<Object>("password length must be higher than 8 characters",HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 		User user = new User();
 		user.setUsername(createUserRequest.getUsername());
@@ -56,6 +66,7 @@ public class UserController {
 		cartRepository.save(cart);
 		user.setCart(cart);
 		userRepository.save(user);
+		log.info("User "+ createUserRequest.getUsername()+" has been created succesfully");
 		return ResponseEntity.ok(user);
 	}
 	
